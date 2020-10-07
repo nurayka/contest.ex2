6294e49781f1c3c4299717fbfe4999ae4e1ad21b
nurayka <62371468+nurayka@users.noreply.github.com>
2020-10-07 20:52:12 +0600

    Update Solution.scala

--- Solution.scala
+++ Solution.scala
@@ -1,15 +1,16 @@
 object Solution {
   def tribonacci(n: Int): Int = {
-val dp=Array(0,1,1)
-    if (n<=2){
-    return dp(n)
-    }
-    for (i<-3 until(n)){
-val a=dp(0)+dp(1)+dp(1)
-      dp(0)=dp(1)
-      dp(1)=dp(2)
-      dp(2)=a
-    }
-    dp(2)
+ if (n==0){return n}
+  var t0=0;
+  var t1=1;
+  var t2=1;
+  var t=1;
+  for(i<-3 to n ){
+    t=t0+t1+t2;
+    t0=t1;
+    t1=t2;
+    t2=t;
+  }
+  t
   }
 }
